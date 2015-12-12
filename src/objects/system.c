27e1ce1f41e0385cafddbf0d8901c35e0aacd23f
metredigm <jtstanley9@gmail.com>
2015-12-11 20:04:44 -0600

    Added FPS display

--- src/objects/system.c
+++ src/objects/system.c
@@ -5,6 +5,7 @@
 #include <tds/memory.h>
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 struct tds_object_type obj_system_type = {
@@ -12,11 +13,11 @@ struct tds_object_type obj_system_type = {
 	.default_sprite = (const char*) 0,
 	.default_params = (struct tds_object_param*) 0,
 	.default_params_size = 0,
-	.data_size = 0,
+	.data_size = sizeof (struct obj_system_data),
 	.func_init = obj_system_init,
 	.func_destroy = obj_system_destroy,
 	.func_update = obj_system_update,
-	.func_draw = (void*) 0,
+	.func_draw = obj_system_draw,
 	.func_msg = (void*) 0,
 	.func_import = obj_system_import,
 	.func_export = obj_system_export,
@@ -31,6 +32,24 @@ void obj_system_destroy(struct tds_object* ptr) {
 }
 
 void obj_system_update(struct tds_object* ptr) {
+}
+
+void obj_system_draw(struct tds_object* ptr) {
+	struct obj_system_data* data = (struct obj_system_data*) ptr->object_data;
+	struct tds_text_batch tb = {0};
+
+	tb.str = data->fps_str;
+	tb.str_len = sizeof data->fps_str / sizeof *(data->fps_str);
+	tb.font = tds_sprite_cache_get(ptr->smgr, "font_debug");
+
+	tb.layer = 1000;
+	tb.r = tb.g = tb.b = tb.a = 1.0f;
+	tb.x = tds_engine_global->camera_handle->x - tds_engine_global->camera_handle->width / 2.0f + 1.0f;
+	tb.y = tds_engine_global->camera_handle->y - tds_engine_global->camera_handle->height / 2.0f + 1.0f;
+
+	snprintf(data->fps_str, sizeof data->fps_str / sizeof *(data->fps_str), "FPS %f", tds_engine_global->state.fps);
+
+	tds_text_submit(tds_engine_global->text_handle, &tb);
 }
 
 void obj_system_import(struct tds_object* ptr, struct tds_object_param* param_list) {
--- src/objects/system.h
+++ src/objects/system.h
@@ -5,7 +5,12 @@
 extern struct tds_object_type obj_system_type;
 
 void obj_system_update(struct tds_object* ptr);
+void obj_system_draw(struct tds_object* ptr);
 void obj_system_init(struct tds_object* ptr);
 void obj_system_destroy(struct tds_object* ptr);
 void obj_system_import(struct tds_object* ptr, struct tds_object_param* param_list);
 struct tds_object_param* obj_system_export(struct tds_object* ptr);
+
+struct obj_system_data {
+	char fps_str[64];
+};
