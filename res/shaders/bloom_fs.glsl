f6af19b7b87ebf0ac78be6da6faecef1baf944e7
metredigm <jtstanley9@gmail.com>
2016-02-01 13:29:51 -0600

    Improved bloom, blur shaders, made sun brighter

--- res/shaders/bloom_fs.glsl
+++ res/shaders/bloom_fs.glsl
@@ -11,7 +11,7 @@ void main(void) {
 	vec4 inp_color = texture2D(tds_texture, p_texcoord) * tds_color;
 
 	/* More gaussian weight black magic */
-	if (dot(inp_color, 0.2126, 0.7152, 0.0722) > 1.0f) {
+	if (dot(inp_color.xyz, vec3(0.2126, 0.7152, 0.0722)) > 0.75f) {
 		color = inp_color;
 	} else {
 		discard;
--- res/shaders/hblur_vs.glsl
+++ res/shaders/hblur_vs.glsl
@@ -14,18 +14,18 @@ void main(void) {
 
 	/* Precompute the texture coordinates for the blur; this greatly increases shader performance */
 
-	p_texcoord_blur[0] = p_texcoord + vec2(-0.028f, 0.0f);
-	p_texcoord_blur[1] = p_texcoord + vec2(-0.024f, 0.0f);
-	p_texcoord_blur[2] = p_texcoord + vec2(-0.020f, 0.0f);
-	p_texcoord_blur[3] = p_texcoord + vec2(-0.016f, 0.0f);
-	p_texcoord_blur[4] = p_texcoord + vec2(-0.012f, 0.0f);
-	p_texcoord_blur[5] = p_texcoord + vec2(-0.008f, 0.0f);
-	p_texcoord_blur[6] = p_texcoord + vec2(-0.004f, 0.0f);
-	p_texcoord_blur[7] = p_texcoord + vec2(0.004f, 0.0f);
-	p_texcoord_blur[8] = p_texcoord + vec2(0.008f, 0.0f);
-	p_texcoord_blur[9] = p_texcoord + vec2(0.012f, 0.0f);
-	p_texcoord_blur[10] = p_texcoord + vec2(0.016f, 0.0f);
-	p_texcoord_blur[11] = p_texcoord + vec2(0.020f, 0.0f);
-	p_texcoord_blur[12] = p_texcoord + vec2(0.024f, 0.0f);
-	p_texcoord_blur[13] = p_texcoord + vec2(0.028f, 0.0f);
+	p_texcoord_blur[0] = p_texcoord + vec2(-0.014f, 0.0f);
+	p_texcoord_blur[1] = p_texcoord + vec2(-0.012f, 0.0f);
+	p_texcoord_blur[2] = p_texcoord + vec2(-0.010f, 0.0f);
+	p_texcoord_blur[3] = p_texcoord + vec2(-0.008f, 0.0f);
+	p_texcoord_blur[4] = p_texcoord + vec2(-0.006f, 0.0f);
+	p_texcoord_blur[5] = p_texcoord + vec2(-0.004f, 0.0f);
+	p_texcoord_blur[6] = p_texcoord + vec2(-0.002f, 0.0f);
+	p_texcoord_blur[7] = p_texcoord + vec2(0.002f, 0.0f);
+	p_texcoord_blur[8] = p_texcoord + vec2(0.004f, 0.0f);
+	p_texcoord_blur[9] = p_texcoord + vec2(0.006f, 0.0f);
+	p_texcoord_blur[10] = p_texcoord + vec2(0.008f, 0.0f);
+	p_texcoord_blur[11] = p_texcoord + vec2(0.010f, 0.0f);
+	p_texcoord_blur[12] = p_texcoord + vec2(0.012f, 0.0f);
+	p_texcoord_blur[13] = p_texcoord + vec2(0.014f, 0.0f);
 }
